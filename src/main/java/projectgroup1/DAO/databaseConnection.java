444e8fd22efac89f5b001b92bd424ee0cd675f7d
Mr.Ant <enthtooaung1234@gmail.com>
2025-12-13 20:34:49 +0630

    creation of the basic frame

--- src/main/java/projectgroup1/DAO/databaseConnection.java
+++ src/main/java/projectgroup1/DAO/databaseConnection.java
@@ -1,5 +1,28 @@
 package projectgroup1.DAO;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class databaseConnection {
+    private static HikariDataSource dataSource;
+
+    static{
+        HikariConfig config =new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/salesystem");
+        config.setUsername("root");
+        config.setPassword("123456");
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 
 }
