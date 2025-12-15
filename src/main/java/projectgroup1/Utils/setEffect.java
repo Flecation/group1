dbd6a66006fafea58b3a3f8b961840f4078d87a9
Mr.Ant <enthtooaung1234@gmail.com>
2025-12-15 23:31:22 +0630

    creation of the basic frame

--- src/main/java/projectgroup1/Utils/setEffect.java
+++ src/main/java/projectgroup1/Utils/setEffect.java
@@ -1,4 +1,13 @@
 package projectgroup1.Utils;
 
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
 public class setEffect {
+
+        public static void setTitleBar(Parent basePane, Button minimizeBtn, Button restoreBtn, Button exitBtn) {
+            titleBar.setTitleBar(basePane, minimizeBtn, restoreBtn, exitBtn);
+        }
+
+
 }
