7db2b30a5375efb588b9be522c7816da3fd858e7
Mr.Ant <enthtooaung1234@gmail.com>
2025-12-16 09:39:47 +0630

    creation of the basic frame

--- src/main/java/projectgroup1/DAO/databaseConnection.java
+++ src/main/java/projectgroup1/DAO/databaseConnection.java
@@ -11,15 +11,13 @@ public class databaseConnection {
     private static HikariDataSource dataSource;
 
 //    This is for the every one of the database name
-    private static String url = "jdbc:mysql://localhost:3306/";
-    private static String username = "root";
-    private static String password = "123456";
+
 
     static{
         HikariConfig config =new HikariConfig();
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
+        config.setJdbcUrl(dotenv.mysql_url);
+        config.setUsername(dotenv.mysql_username);
+        config.setPassword(dotenv.mysql_password);
 
         config.setMaximumPoolSize(10);
         config.setMinimumIdle(2);
---
+++ src/main/java/projectgroup1/DAO/dotenv.java
@@ -0,0 +1,13 @@
+package projectgroup1.DAO;
+
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class dotenv {
+    static Dotenv dotenv = Dotenv.load();
+
+    public static String mysql_username = dotenv.get("MYSQL_USERNAME");
+    public static String mysql_password = dotenv.get("MYSQL_PASSWORD");
+    public static String mysql_url = dotenv.get("MYSQL_URL");
+
+}
