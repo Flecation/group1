8a7a1d395212f6479fc8195f1626330f537530e3
Mr.Ant <enthtooaung1234@gmail.com>
2025-12-26 12:59:57 +0630

    creation of the basic frame

--- src/main/java/IPPSystem/Controllers/loginController.java
+++ src/main/java/IPPSystem/Controllers/loginController.java
@@ -3,6 +3,7 @@ package IPPSystem.Controllers;
 import IPPSystem.Utils.effect;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 
 public class loginController {
@@ -32,5 +33,42 @@ public class loginController {
         restoreBtn.setDisable(true);
         effect.setFloatTextFieldStyle(userNameLbl,userNameTxt);
         effect.setFloatPasswordFieldStyle(passwordLbl,showPasswordTxt,hidePasswordTxt);
+
+        userNameTxt.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER){
+                if(showPasswordTxt.isVisible()){
+                    showPasswordTxt.requestFocus();
+                }else {
+                    hidePasswordTxt.requestFocus();
+                }
+            }
+            if (keyEvent.getCode() == KeyCode.DOWN){
+                if(showPasswordTxt.isVisible()){
+                    showPasswordTxt.requestFocus();
+                }else {
+                    hidePasswordTxt.requestFocus();
+                }
+            }else{
+
+            }
+        });
+
+        showPasswordTxt.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER){
+                loginBtn.fire();
+            }
+            if (keyEvent.getCode() == KeyCode.UP){
+                userNameTxt.requestFocus();
+            }
+        });
+
+        hidePasswordTxt.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER){
+                loginBtn.fire();
+            }
+            if (keyEvent.getCode() == KeyCode.UP){
+                userNameTxt.requestFocus();
+            }
+        });
     }
 }
